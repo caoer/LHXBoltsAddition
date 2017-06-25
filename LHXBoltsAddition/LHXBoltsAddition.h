e11e714bb1c1186c354571480f4766122123c93f
Zitao Xiong <caoer115@gmail.com>
2017-06-25 06:42:11 -0400

    add carthnage

---
+++ LHXBoltsAddition/LHXBoltsAddition.h
@@ -0,0 +1,19 @@
+//
+//  LHXBoltsAddition.h
+//  LHXBoltsAddition
+//
+//  Created by Zitao Xiong on 6/25/17.
+//  Copyright © 2017 Zitao Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LHXBoltsAddition.
+FOUNDATION_EXPORT double LHXBoltsAdditionVersionNumber;
+
+//! Project version string for LHXBoltsAddition.
+FOUNDATION_EXPORT const unsigned char LHXBoltsAdditionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHXBoltsAddition/PublicHeader.h>
+
+
